c1c44af28989f6e2499ce4a9cac3d164cf5074f4
unknown <anrodriguez@Tic-pc-150.dgcp.local>
2016-12-13 15:56:50 -0400

    working on the signup form

--- web/src/components/Signup.js
+++ web/src/components/Signup.js
@@ -1,31 +1,90 @@
-class Signup extends React.Componenet{
-constructor()
-{
-  super();
-  this.state ={
-    fullName:''
+import React from 'react';
+
+function Location(props){
+
+  return(
+  <div>
+    <div>
+        <label>Lat</label>
+        <input type="text" value={props.lat} onChange={() => this.props.onChange()}/>
+    </div>
+    <div>
+        <label>Lng</label>
+        <input type="text" value={props.lng}/>
+    </div>
+    <div>
+        <label>Country</label>
+        <input type="text" value={props.country}/>
+    </div>
+    <div>
+       <label>City</label>
+       <input type="text" value={props.city}/>
+    </div>
+    <div>
+       <label>Time Zone</label>
+       <input type="text" value={props.timeZone}/>
+     </div>
+  </div>
+
+  );
+
+
+}
+
+class Signup extends React.Component{
+  constructor()
+  {
+    super();
+    this.state ={
+        id:'',
+        fullName:'',
+        cohortName:'',
+        userName:'',
+        email:'',
+        profileImage:'',
+        lat:'',
+        lng:'',
+        country:'',
+        city:'',
+        timeZone:''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  this.handleChange = this.handleChange.bind(this);
-  this.handleSubmit = this.handleSubmit.bind(this);
-}
 
- handleChange(event){
-  this.setState({ fullName = event.target.fullName});
-}
+   handleChange(event){
+     console.log(event);
+    this.setState({ fullName: event.target.value});
+  }
 
- handleSubmit(event){
-  console.log("My submit "+ this.state.fullName);
-  event.preventDefault();
-}
+   handleSubmit(event){
+    alert("My submit "+ this.state.person.fullName + ' ' +this.state.person.cohortName);
+    event.preventDefault();
+  }
 
 render(){
   return (
-    <form>
+    <form onSubmit={this.handleSubmit}>
        <div>
           <label>Full Name</label>
-          <input type="text" value={this.stat.fullName} onChange={this.handleChange}/>
+          <input type="text" value={this.state.fullName} onChange={(event) => {this.setState({fullName:event.target.value })}}/>
        </div>
-    <form>
+       <div>
+          <label>CoHort Name</label>
+          <select value={this.state.cohorName} onChange={(event) => {this.setState({cohortName: event.target.value})}}>
+            <option value="redPanda">Red panda</option>
+            <option value="articFox">Artic fox</option>
+            <option value="cheetah">Cheetah</option>
+            <option value="kangaroo">Kangaroo</option>
+            <option value="racoon">Racoon</option>
+            <option value="rhino">Rhino</option>
+          </select>
+          </div>
+           <Location onChange={(event) => {this.setState({})} }/>
+       <div><input type="submit" value="Send"/></div>
+    </form>
   );
 }
 }
+
+export default Signup;
--- web/src/index.js
+++ web/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Signup from './components/Signup';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Signup />,
   document.getElementById('signup')
 );
